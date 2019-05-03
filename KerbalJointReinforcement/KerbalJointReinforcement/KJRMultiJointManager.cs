bb905621d330fe2c2fdbce9fba5e801e823b1176
meirumeiru <33137857+meirumeiru@users.noreply.github.com>
2019-05-03 23:49:55 +0200

    v4.0
    first checkin of new project

--- KerbalJointReinforcement/KerbalJointReinforcement/KJRMultiJointManager.cs
+++ KerbalJointReinforcement/KerbalJointReinforcement/KJRMultiJointManager.cs
@@ -1,228 +1,178 @@
-﻿/*
-Kerbal Joint Reinforcement, v3.3.3
-Copyright 2015, Michael Ferrara, aka Ferram4
-
-    This file is part of Kerbal Joint Reinforcement.
-
-    Kerbal Joint Reinforcement is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
-
-    Kerbal Joint Reinforcement is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with Kerbal Joint Reinforcement.  If not, see <http://www.gnu.org/licenses/>.
-*/
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 
 namespace KerbalJointReinforcement
 {
-    //All this class exists to do is to act as a box attached to 
-    //For a sequence of three parts (A, B, C), connected in series, this will exist on B and hold the strengthening joint from A to C
-    //If the joint from A to B or B to C is broken, this will destroy the joint A to C and then destroy itself
-    class KJRMultiJointManager
-    {
-        public static KJRMultiJointManager fetch;
+	//All this class exists to do is to act as a box attached to 
+	//For a sequence of three parts (A, B, C), connected in series, this will exist on B and hold the strengthening joint from A to C
+	//If the joint from A to B or B to C is broken, this will destroy the joint A to C and then destroy itself
+	internal class KJRMultiJointManager
+	{
+		internal enum Reason
+		{
+			None, AdditionalJointToParent, MultiPartJointTreeChildren, MultiPartJointTreeChildrenRoot
+		};
 
-        Dictionary<Part, List<ConfigurableJoint>> multiJointDict;
-        List<Part> linkPart1List;
-        List<Part> linkPart2List;
-        HashSet<Part> linkedSet;
+		Dictionary<Part, List<ConfigurableJoint>> multiJointDict;
+		List<Part> linkedSet;
+		List<Part> tempPartList;
 
-        public KJRMultiJointManager()
-        {
-            multiJointDict = new Dictionary<Part, List<ConfigurableJoint>>();
-            linkPart1List = new List<Part>();
-            linkPart2List = new List<Part>();
-            linkedSet = new HashSet<Part>();
-            fetch = this;
-            GameEvents.onVesselCreate.Add(VesselCreate);
-            GameEvents.onPartUndock.Add(OnJointBreak);
-            GameEvents.onPartDie.Add(OnJointBreak);
-        }
+		Dictionary<ConfigurableJoint, Reason> jointReasonDict;
 
-        public void OnDestroy()
-        {
-            Debug.Log("KJRMultiJointManager cleanup");
-            GameEvents.onVesselCreate.Remove(VesselCreate);
-            GameEvents.onPartUndock.Remove(OnJointBreak);
-            GameEvents.onPartDie.Remove(OnJointBreak);
-        }
-        
-        //This entire scheme relies on a simple fact: when a vessel is created, the part that was decoupled is the root part
-        //Therefore, we only need to use vessel.RootPart and the parts with no children to ensure that all multijoints are broken
-        private void VesselCreate(Vessel v)
-        {
-            //Debug.Log(v.name + " joint break");
-            OnJointBreak(v.rootPart);
+		public KJRMultiJointManager()
+		{
+			multiJointDict = new Dictionary<Part, List<ConfigurableJoint>>();
+			linkedSet = new List<Part>();
+			tempPartList = new List<Part>();
+			jointReasonDict = new Dictionary<ConfigurableJoint,Reason>();
+		}
 
-            for(int i = 0; i < v.Parts.Count; ++i)
-            {
-                Part p = v.Parts[i];
-                OnJointBreak(p);
-            }
-        }
+		public void RemoveAllVesselJoints(Vessel v)
+		{
+			if(v.loaded)
+			{
+				foreach(Part p in v.Parts)
+					RemovePartJoints(p);
+			}
+		}
 
-        public bool TrySetValidLinkedSet(Part linkPart1, Part linkPart2)
-        {
-            linkPart1List.Clear();
-            linkPart2List.Clear();
-            linkedSet.Clear();
+		public bool TrySetValidLinkedSet(Part linkPart1, Part linkPart2)
+		{
+			linkedSet.Clear();
+			tempPartList.Clear();
 
-            if (!KJRJointUtils.JointAdjustmentValid(linkPart1) || !KJRJointUtils.JointAdjustmentValid(linkPart2))
-                return false;
+			while(linkPart1 != null)
+			{
+				linkedSet.Add(linkPart1);
+				linkPart1 = KJRJointUtils.IsJointAdjustmentAllowed(linkPart1) ? linkPart1.parent : null;
+			}
 
-            //Add the parts we're connecting to their respective lists
-            linkPart1List.Add(linkPart1);
-            linkPart2List.Add(linkPart2);
+			while(linkPart2 != null)
+			{
+				tempPartList.Add(linkPart2);
+				linkPart2 = KJRJointUtils.IsJointAdjustmentAllowed(linkPart2) ? linkPart2.parent : null;
+			}
 
-            //iterate through each part's parents, making them the new linkPart and then adding them to their respective lists.  LinkPart.parent will not be null until it reaches the root part
-            while ((object)linkPart1.parent != null)
-            {
-                linkPart1 = linkPart1.parent;
-                linkPart1List.Add(linkPart1);
-            }
-            while ((object)linkPart2.parent != null)
-            {
-                linkPart2 = linkPart2.parent;
-                linkPart2List.Add(linkPart2);
-            }
+			int i = linkedSet.Count - 1;
+			int j = tempPartList.Count - 1;
 
-            int index1, index2;
-            index1 = linkPart1List.Count - 1;
-            index2 = linkPart2List.Count - 1;
-            bool reachedMergeNode = false;
+			if(linkedSet[i] != tempPartList[j])
+				return false; // not same root, so they can never be in a valid set
 
-            //Start iterating through the lists, starting from the last indices.  Due to the way the lists were created, index 0 = connected part, while index = Count - 1 = root part
-            while (true)
-            {
-                Part p = null, q = null;
+			while((i >= 0) && (j >= 0) && (linkedSet[i] == tempPartList[j]))
+			{ --i; --j; }
 
-                //if the indices are valid, get those particular parts
-                if (index1 >= 0)
-                    p = linkPart1List[index1];
-                if (index2 >= 0)
-                    q = linkPart2List[index2];
+			linkedSet.AddRange(tempPartList.GetRange(0, j + 1)); 
 
-                //decrement indices for next iteration
-                index1--;
-                index2--;
+			return linkedSet.Count > 1;
+		}
 
-                //if p and q are the same, this is the merge we care about; set variable noting this now
-                if (p == q)
-                    reachedMergeNode = true;
+		public void RegisterMultiJointBetweenParts(Part linkPart1, Part linkPart2, ConfigurableJoint multiJoint, Reason jointReason)
+		{
+			foreach(Part p in linkedSet)
+				RegisterMultiJoint(p, multiJoint, jointReason);
+		}
 
-                //if we haven't reached the merge yet, go back to the beginning of the loop
-                if (!reachedMergeNode)
-                    continue;
+		public void RegisterMultiJoint(Part testPart, ConfigurableJoint multiJoint, Reason jointReason)
+		{
+			List<ConfigurableJoint> configJointList;
+			if(multiJointDict.TryGetValue(testPart, out configJointList))
+			{
+				for(int i = configJointList.Count - 1; i >= 0; --i)
+					if(configJointList[i] == null)
+						configJointList.RemoveAt(i);
 
-                //if we have reached the merge, all the remaining parts are parts of the line connecting these parts and should be set to be disconnected
-                if ((object)p != null)
-                {
-                    if (KJRJointUtils.JointAdjustmentValid(p))
-                        linkedSet.Add(p);
-                    else
-                        return false;
-                }
-                if ((object)q != null)
-                {
-                    if (KJRJointUtils.JointAdjustmentValid(q))
-                        linkedSet.Add(q);
-                    else
-                        return false;
-                }
+				configJointList.Add(multiJoint);
+			}
+			else
+			{
+				configJointList = new List<ConfigurableJoint>();
+				configJointList.Add(multiJoint);
+				multiJointDict.Add(testPart, configJointList);
+			}
 
-                //if both p and q are null, that means we've reached the end of both lines and so all the parts have been added
-                if ((object)p == null && (object)q == null)
-                    break;
-            }
+			if(!jointReasonDict.ContainsKey(multiJoint))
+				jointReasonDict.Add(multiJoint, jointReason);
+		}
 
-            return true;
-        }
+		public bool CheckMultiJointBetweenParts(Part testPart1, Part testPart2)
+		{
+			if(testPart1 == null || testPart2 == null || testPart1 == testPart2)
+				return false;
 
-        public void RegisterMultiJointBetweenParts(Part linkPart1, Part linkPart2, ConfigurableJoint multiJoint)
-        {
-            foreach (Part p in linkedSet)
-                RegisterMultiJoint(p, multiJoint);
-        }
+			List<ConfigurableJoint> testMultiJoints;
 
-        public void RegisterMultiJoint(Part testPart, ConfigurableJoint multiJoint)
-        {
-            List<ConfigurableJoint> configJointList;
-            if (multiJointDict.TryGetValue(testPart, out configJointList))
-            {
-                for (int i = configJointList.Count - 1; i >= 0; --i)
-                    if (configJointList[i] == null)
-                        configJointList.RemoveAt(i);
+			if(!multiJointDict.TryGetValue(testPart1, out testMultiJoints))
+				return false;
 
-                configJointList.Add(multiJoint);
-            }
-            else
-            {
-                configJointList = new List<ConfigurableJoint>();
-                configJointList.Add(multiJoint);
-                multiJointDict.Add(testPart, configJointList);
-            }
-        }
+			Rigidbody testRb = testPart2.rb;
 
-        public bool CheckMultiJointBetweenParts(Part testPart1, Part testPart2)
-        {
-            if (testPart1 == null || testPart2 == null || testPart1 == testPart2)
-                return false;
+			for(int i = 0; i < testMultiJoints.Count; i++)
+			{
+				ConfigurableJoint joint = testMultiJoints[i];
+				if(joint == null)
+				{
+					testMultiJoints.RemoveAt(i);
+					--i;
+					continue;
+				}
+				if(joint.connectedBody == testRb)
+					return true;
+			}
 
-            List<ConfigurableJoint> testMultiJoints;
+			return false;
+		}
 
-            if (!multiJointDict.TryGetValue(testPart1, out testMultiJoints))
-                return false;
+		public void RemovePartJoints(Part part)
+		{
+			if(part == null)
+				return;
 
-            Rigidbody testRb = testPart2.rb;
+			List<ConfigurableJoint> configJointList;
+			if(multiJointDict.TryGetValue(part, out configJointList))
+			{
+				for(int i = 0; i < configJointList.Count; i++)
+				{
+					ConfigurableJoint joint = configJointList[i];
+	
+					jointReasonDict.Remove(joint);
 
-            for (int i = 0; i < testMultiJoints.Count; i++)
-            {
-                ConfigurableJoint joint = testMultiJoints[i];
-                if (joint == null)
-                {
-                    testMultiJoints.RemoveAt(i);
-                    --i;
-                    continue;
-                }
-                if (joint.connectedBody == testRb)
-                    return true;
-            }
+					if(joint != null)
+						GameObject.Destroy(joint);
+				}
 
-            return false;
-        }
+				multiJointDict.Remove(part);
+			}
+		}
 
-        public void OnJointBreak(PartJoint partJoint)
-        {
-            OnJointBreak(partJoint.Parent);
-        }
+#if IncludeAnalyzer
+		internal ConfigurableJoint[] GetAllJoints()
+		{
+			HashSet<ConfigurableJoint> l = new HashSet<ConfigurableJoint>();
 
-        public void OnJointBreak(Part part)
-        {
-            if (part == null)
-                return;
-            List<ConfigurableJoint> configJointList;
-            if (multiJointDict.TryGetValue(part, out configJointList))
-            {
-                for(int i = 0; i < configJointList.Count; i++)
-                {
-                    ConfigurableJoint joint = configJointList[i];
-                    if (joint != null)
-                    {
-                        GameObject.Destroy(joint);
-                    }
-                }
+			Dictionary<Part, List<ConfigurableJoint>>.Enumerator e = multiJointDict.GetEnumerator();
 
-                multiJointDict.Remove(part);
-            }
-        }
-    }
+			while(e.MoveNext())
+			{
+				List<ConfigurableJoint> j = e.Current.Value;
+
+				for(int a = 0; a < j.Count; a++)
+					l.Add(j[a]);
+			}
+
+			return l.ToArray();
+		}
+#endif
+
+		internal Reason GetJointReason(ConfigurableJoint joint)
+		{
+			Reason jointReason;
+			if(jointReasonDict.TryGetValue(joint, out jointReason))
+				return jointReason;
+
+			return Reason.None;
+		}
+	}
 }
