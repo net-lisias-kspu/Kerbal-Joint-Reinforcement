7b4856ce34a3ddef20d324e69f39d8343bbc26db
meirumeiru <33137857+meirumeiru@users.noreply.github.com>
2022-03-21 02:01:12 +0100

    small changes
    fix for update

--- KerbalJointReinforcement/KerbalJointReinforcement/Properties/AssemblyInfo.cs
+++ KerbalJointReinforcement/KerbalJointReinforcement/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Kerbal Joint Reinforcement Next")]
-[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("4.1.15.0")]
-[assembly: AssemblyFileVersion("4.1.15.0")]
+[assembly: AssemblyVersion("4.1.18.0")]
+[assembly: AssemblyFileVersion("4.1.18.0")]
